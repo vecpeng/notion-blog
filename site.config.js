ef2a842b8403d4e54c0b05aad7da77e08e2c3513
vecpeng <841298391@qq.com>
2022-03-29 13:47:41 +0800

    chore: finish config

--- site.config.js
+++ site.config.js
@@ -1,25 +1,35 @@
+/*
+ * @Author: vecpeng
+ * @Date: 2022-03-29 12:39:39
+ * @LastEditors: vecpeng
+ * @LastEditTime: 2022-03-29 13:34:06
+ * @FilePath: /nextjs-notion-starter-kit/site.config.js
+ * @Desc: 
+ * 
+ * Copyright (c) 2022 by vecpeng, All Rights Reserved. 
+ */
 export default {
   // where it all starts -- the site's root Notion page (required)
-  rootNotionPageId: '78fc5a4b88d74b0e824e29407e9f1ec1',
+  rootNotionPageId: '6911b004390e4ef6be2253178ba1a0ec',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Transitive Bullshit',
-  domain: 'transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: 'vecpeng',
+  domain: 'blog.vecpeng.com',
+  author: 'vecpeng',
 
   // open graph metadata (optional)
-  description: 'Example site description',
-  socialImageTitle: 'Transitive Bullshit',
+  description: 'Stay foolish, stay hungry',
+  socialImageTitle: '',
   socialImageSubtitle: 'Hello World! 👋',
 
   // social usernames (optional)
-  twitter: 'transitive_bs',
-  github: 'transitive-bullshit',
-  linkedin: 'fisch2',
+  twitter: 'vecpeng',
+  github: 'vecpeng',
+  linkedin: 'vecpeng',
 
   // default notion icon and cover images for site-wide consistency (optional)
   // page-specific values will override these site-wide defaults
